a440938237143cfde97179c58639b9a611186ec4
sekarmano14 <sekarmano46@gmail.com>
2024-05-01 08:55:47 +0530

    Kumar's new Code

--- src/test/java/org/test/TestMain.java
+++ src/test/java/org/test/TestMain.java
@@ -1,5 +1,13 @@
 package org.test;
 
+import org.testng.annotations.Test;
+
 public class TestMain {
+	@Test
+	private void test() {
+		System.out.println("Test3");
+		System.out.println("Test4");
+
+	}
 
 }
